dd79f9736de3348d5693fe4375075e079562d1c6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-22 11:56:28 +0300

    Get Category Command Handler Class

---
+++ Core/MovieApi.Application/Features/QCRSDesignPattern/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.QCRSDesignPattern.Results.CategoryResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.QCRSDesignPattern.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetCategoryQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var values = await _context.Categories.ToListAsync();
+            return values.Select(x => new GetCategoryQueryResult
+            {
+                CategoryID = x.CategoryID,
+                CategoryName = x.CategoryName
+            }).ToList();
+        }
+    }
+}
