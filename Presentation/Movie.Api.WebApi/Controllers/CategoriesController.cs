5b34bcb002f5accf3c27e15aa198eacea49003f3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-22 11:56:52 +0300

    Categories API  Controller Added

---
+++ Presentation/Movie.Api.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+       
+    }
+}
