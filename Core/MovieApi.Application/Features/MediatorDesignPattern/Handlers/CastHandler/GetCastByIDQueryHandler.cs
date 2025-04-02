c9bd3de241395cd8deefe44321c9d968354ffb17
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-02 19:02:05 +0300

    Mediator Cast Query Handler Process Added

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandler/GetCastByIDQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandler/GetCastByIDQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandler
 {
-    class GetCastByIDQueryHandler
+    public class GetCastByIDQueryHandler : IRequestHandler<GetCastByIDQuery, GetCastByIDQueryResult>
     {
+        private readonly MovieContext _movieContext;
+
+        public GetCastByIDQueryHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task<GetCastByIDQueryResult> Handle(GetCastByIDQuery request, CancellationToken cancellationToken)
+        {
+            var values =await _movieContext.Casts.FindAsync(request.CastID);
+            return new GetCastByIDQueryResult
+            {
+                CastID = values.CastID,
+                CastBiography = values.CastBiography,
+                CastImageURL = values.CastImageURL,
+                CastName = values.CastName,
+                CastOverview = values.CastOverview,
+                CastSurname = values.CastSurname,
+                CastTitle = values.CastTitle
+
+            };
+        }
     }
 }
--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandler/GetCastQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandler/GetCastQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,31 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandler
 {
-    class GetCastQueryHandler
+    public class GetCastQueryHandler : IRequestHandler<GetCastQuery, List<GetCastQueryResult>>
     {
+        private readonly MovieContext _movieContext;
+
+        public GetCastQueryHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task<List<GetCastQueryResult>> Handle(GetCastQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _movieContext.Casts.ToListAsync();
+            return values.Select(x=>new GetCastQueryResult
+            {
+                CastID = x.CastID,
+                CastBiography = x.CastBiography,
+                CastImageURL = x.CastImageURL,
+                CastName = x.CastName,
+                CastOverview = x.CastOverview,
+                CastSurname = x.CastSurname,
+                CastTitle = x.CastTitle
+                
+            }).ToList();
+        } //AsNoTracking: üzerinde sadece read yapılacak işlemlerde kullandığımız bir ef core fonksiyonudur.
+          //Normalde ef core entity üzerinde tracking başlatır ve yapılan değişikleri takip ederek savechanges ile veritabanına yansıtır.
+          //Ama read işleminde savechanges gerek olmadığı ve entity üzerinde bir güncelleme yapmadığımız için performans artışı için AsNoTracking kullanırız.
     }
 }
