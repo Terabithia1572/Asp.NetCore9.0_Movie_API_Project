701cab9cb5b8da662f79d38d4089493a7e9dfb26
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-02 18:35:09 +0300

    Commands Queries Results Classes added with MediatR

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/CastCommands/UpdateCastCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands
+{
+    public class UpdateCastCommand:IRequest
+    {
+        //public int CastID { get; set; } //Oyuncu ID
+        public string CastTitle { get; set; } //Oyuncu Başlık
+        public string CastName { get; set; } //Oyuncu Ad
+        public string CastSurname { get; set; } //Oyuncu Soyad
+        public string CastImageURL { get; set; } //Oyuncu Resim URL
+        public string? CastOverview { get; set; } //Oyuncu Genel bakış
+        public string? CastBiography { get; set; } //Oyuncu Biyografisi
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastByIDQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastByIDQuery:IRequest<GetCastByIDQueryResult>
+    {
+        public int CastID { get; set; } //Oyuncu ID
+
+        public GetCastByIDQuery(int castID)
+        {
+            CastID = castID;
+        }
+    }
+}
