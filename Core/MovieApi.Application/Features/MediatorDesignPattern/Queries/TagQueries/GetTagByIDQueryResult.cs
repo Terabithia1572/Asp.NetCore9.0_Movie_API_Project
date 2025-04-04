a3c90cd83064dacb6e8bc53c2da6d9d5ea871d7b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-04 15:15:48 +0300

    Tag Queries Process Added

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIDQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagByIDQueryResult:IRequest<GetTagByIDQueryResult>
+    {
+        public int TagID { get; set; } //Etiket ID
+
+        public GetTagByIDQueryResult(int tagID)
+        {
+            TagID = tagID;
+        }
+    }
+}
