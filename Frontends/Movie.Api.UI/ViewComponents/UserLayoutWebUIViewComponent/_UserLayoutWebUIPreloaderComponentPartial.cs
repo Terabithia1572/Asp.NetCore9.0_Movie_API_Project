50048b5f80d1f241c7d6706eaac47649054d8ec8
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-10 21:32:54 +0300

    24# Login and Register Modal Arrangements Process Added

---
+++ Frontends/Movie.Api.UI/ViewComponents/UserLayoutWebUIViewComponent/_UserLayoutWebUIPreloaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.UI.ViewComponents.UserLayoutWebUIViewComponent
+{
+    public class _UserLayoutWebUIPreloaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
