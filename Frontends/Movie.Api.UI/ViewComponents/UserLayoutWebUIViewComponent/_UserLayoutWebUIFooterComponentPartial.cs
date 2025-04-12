1731346df1de0f60128d86b8316c367512007145
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-12 14:19:16 +0300

    27# Completion of Layout Operations Process Added

---
+++ Frontends/Movie.Api.UI/ViewComponents/UserLayoutWebUIViewComponent/_UserLayoutWebUIFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.UI.ViewComponents.UserLayoutWebUIViewComponent
+{
+    public class _UserLayoutWebUIFooterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
