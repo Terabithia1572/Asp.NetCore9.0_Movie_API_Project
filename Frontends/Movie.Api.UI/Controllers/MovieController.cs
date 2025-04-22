e3d784c3ead97d261995c0490714fac1c37b2ca3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-22 19:41:41 +0300

    Listing Movies on HomePage Process Added

--- Frontends/Movie.Api.UI/Controllers/MovieController.cs
+++ Frontends/Movie.Api.UI/Controllers/MovieController.cs
@@ -1,15 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.DTOs.DTOs.MovieDTO;
+using Newtonsoft.Json;
 
 namespace Movie.Api.UI.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> MovieList()
         {
             ViewBag.v1= "Film Listesi";
             ViewBag.v2 = "Ana Sayfa";
             ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httpClientFactory.CreateClient(); //burada client oluşturuyoruz
+            var responseMessage = await client.GetAsync("https://localhost:44319/api/Movies"); // buraya api url yazılacak istek yapmak istediğimiz URL'ye istek yapılıyor
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // gelen veriyi string olarak okuyoruz
+                var values=JsonConvert.DeserializeObject<List<ResultMovieDTO>>(jsonData); // gelen veriyi deserialize ediyoruz burada jsonData daki verileri ile ResultMovieDTO eşleşiyor
+                return View(values); // gelen veriyi view'e gönderiyoruz
+
+            }
             return View();
+
         }
     }
 }
---
+++ Frontends/MovieApi.DTOs/DTOs/MovieDTO/ResultMovieDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.DTOs.DTOs.MovieDTO
+{
+    public class ResultMovieDTO
+    {
+        public int MovieID { get; set; } //Film ID
+        public string MovieTitle { get; set; } //Film adı
+        public string MovieCoverImageURL { get; set; } //Film kapak resmi
+        public decimal MovieRating { get; set; } //Film puanı
+        public string MovieDescription { get; set; } //Film açıklaması
+        public int MovieDuration { get; set; } //Film süresi
+        public DateTime MovieReleaseDate { get; set; } //Film yayın tarihi
+        public string MovileCreatedYear { get; set; } //Film çıkış yılı
+        public bool MovieStatus { get; set; } //Film durumu
+    }
+}
