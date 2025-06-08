b940ac50a76f6dbab18bc0e6da1df55245556a14
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-06-08 16:42:04 +0300

    31# Movie Detail Page Updated + New Areas Added

---
+++ Frontends/Movie.Api.UI/ViewComponents/MovieDetailViewComponent/_MovieDetailRateComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.UI.ViewComponents.MovieDetailViewComponent
+{
+    public class _MovieDetailRateComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
