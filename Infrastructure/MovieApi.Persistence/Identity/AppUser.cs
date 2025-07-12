304f25b2ced1625520ea8e5d26ac61e46a106875
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-12 21:13:58 +0300

    #34 Using AspNet Core Identity with Onion Process Added

---
+++ Infrastructure/MovieApi.Persistence/Identity/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Identity
+{
+    public class AppUser:IdentityUser
+    {
+        public string Name { get; set; } //kullanıcıdan alınan isim
+        public string Surname { get; set; } //kullanıcıdan alınan soyisim
+        public string? ImageURL { get; set; } //kullanıcıdan alınan profil resmi
+    }
+}
