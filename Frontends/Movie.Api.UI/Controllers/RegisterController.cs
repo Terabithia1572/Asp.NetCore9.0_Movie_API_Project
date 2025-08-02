ed248e66fc3d051db347904be1d42b965412dd06
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-02 17:48:46 +0300

    36# API Register Process & UI Register Theme Added

---
+++ Frontends/Movie.Api.UI/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.DTOs.DTOs.UserRegisterDTOs;
+
+namespace Movie.Api.UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(CreateUserRegisterDTO createUserRegisterDTO)
+        {
+            return RedirectToAction("SignIn", "Login");
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
