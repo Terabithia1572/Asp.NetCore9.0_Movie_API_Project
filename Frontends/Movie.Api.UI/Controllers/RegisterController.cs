28809b9f60577dbd49dcc3635dc50f2137c18e12
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-02 18:22:39 +0300

    37# API Consume Register Process Added

--- Frontends/Movie.Api.UI/Controllers/RegisterController.cs
+++ Frontends/Movie.Api.UI/Controllers/RegisterController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieApi.DTOs.DTOs.UserRegisterDTOs;
+using Newtonsoft.Json;
 
 namespace Movie.Api.UI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory; 
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         [HttpGet]
         public IActionResult SignUp()
         {
@@ -13,7 +21,21 @@ namespace Movie.Api.UI.Controllers
         [HttpPost]
         public async Task<IActionResult> SignUp(CreateUserRegisterDTO createUserRegisterDTO)
         {
-            return RedirectToAction("SignIn", "Login");
+            var client = _httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(createUserRegisterDTO);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:44319/api/Registers", stringContent);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Kayıt İşlemi Başarısız. Lütfen Tekrar Deneyin.");
+                return View(createUserRegisterDTO);
+            }
+           
         }
         public IActionResult SignIn()
         {
