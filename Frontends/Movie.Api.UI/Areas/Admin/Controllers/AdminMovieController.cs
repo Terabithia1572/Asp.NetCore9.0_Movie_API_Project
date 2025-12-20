124a2f742486c9e6da5e797c995c8fc735522853
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-20 19:51:17 +0300

    #42 Implemented admin-side movie listing with dedicated DTOs and AdminMovieController

---
+++ Frontends/Movie.Api.UI/Areas/Admin/Controllers/AdminMovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.DTOs.DTOs.AdminMovieDTOs;
+using Newtonsoft.Json;
+
+namespace Movie.Api.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMovieController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminMovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
+        {
+            ViewBag.v1 = "Film Listesi";
+            ViewBag.v2 = "Ana Sayfa";
+            ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httpClientFactory.CreateClient(); //burada client oluşturuyoruz
+            var responseMessage = await client.GetAsync("https://localhost:44319/api/Movies"); // buraya api url yazılacak istek yapmak istediğimiz URL'ye istek yapılıyor
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // gelen veriyi string olarak okuyoruz
+                var values = JsonConvert.DeserializeObject<List<AdminResultMovieDTO>>(jsonData); // gelen veriyi deserialize ediyoruz burada jsonData daki verileri ile ResultMovieDTO eşleşiyor
+                return View(values); // gelen veriyi view'e gönderiyoruz
+
+            }
+            return View();
+        }
+    }
+}
