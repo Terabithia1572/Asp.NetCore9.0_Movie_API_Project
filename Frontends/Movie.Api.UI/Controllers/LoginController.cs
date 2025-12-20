0ca17a0b1179d960a9513363a2710da57c208e84
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-20 16:36:41 +0300

    38# Creating a Login Page Design with Stitch AI

---
+++ Frontends/Movie.Api.UI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
