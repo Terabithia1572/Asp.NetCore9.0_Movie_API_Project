f436a48975043c572bb0307b6fc471b726dc1e2d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-20 17:16:08 +0300

    39# Anthropic Claude AI and Admin Layout Editing Process Added

---
+++ Frontends/Movie.Api.UI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Api.UI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
