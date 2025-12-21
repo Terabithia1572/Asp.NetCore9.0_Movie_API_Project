aef03e8c80ef9007cb23619fb017bfe81efff143
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-21 15:00:11 +0300

    44# Listing Categories on the Admin Page

--- Frontends/Movie.Api.UI/Areas/Admin/Controllers/AdminCategoryController.cs
+++ Frontends/Movie.Api.UI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -1,27 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieApi.DTOs.DTOs.AdminCategoryDTOs;
+using MovieApi.DTOs.DTOs.AdminMovieDTOs;
 using Newtonsoft.Json;
 
 namespace Movie.Api.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class AdminMovieController : Controller
+    public class AdminCategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public AdminMovieController(IHttpClientFactory httpClientFactory)
+        public AdminCategoryController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> MovieList()
+        public async Task<IActionResult> CategoryList()
         {
-            ViewBag.v1 = "Film Listesi";
-            ViewBag.v2 = "Ana Sayfa";
-            ViewBag.v3 = "Tüm Filmler";
-
             var client = _httpClientFactory.CreateClient(); //burada client oluşturuyoruz
-            var responseMessage = await client.GetAsync("https://localhost:44319/api/Movies"); // buraya api url yazılacak istek yapmak istediğimiz URL'ye istek yapılıyor
+            var responseMessage = await client.GetAsync("https://localhost:44319/api/Categories"); // buraya api url yazılacak istek yapmak istediğimiz URL'ye istek yapılıyor
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync(); // gelen veriyi string olarak okuyoruz
---
+++ Frontends/MovieApi.DTOs/DTOs/AdminCategoryDTOs/AdminResultCategoryDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.DTOs.DTOs.AdminCategoryDTOs
+{
+    public class AdminResultCategoryDTO
+    {
+        public int CategoryID { get; set; } //hangi kategoriyi getireceksek onun id'si
+        public string CategoryName { get; set; } //hangi kategoriyi getireceksek onun adı
+        public int MovieCount { get; set; } //o kategoride kaç film var
+        public int ReviewCount { get; set; } //o kategorideki filmlere kaç tane yorum yapılmış
+        public double AvgRating { get; set; } //o kategorideki filmlerin ortalama puanı
+        public bool IsActive { get; set; } //o kategorinin aktif mi pasif mi olduğunu gösterir
+    }
+}
